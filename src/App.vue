7586f17dbcbe4d9cf3f69c3500f63609046a63b9
melik-bagriyanik <melikbagriyanik90@gmail.com>
2025-08-15 13:37:08 +0300

    added header

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,12 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import BaseButton from './components/atoms/BaseButton.vue'
+import Header from './components/organisms/Header.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <BaseButton variant="secondary">Click Me</BaseButton>
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Header />
+  <main></main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,59 @@
+<!-- src/components/organisms/Navbar.vue -->
+<template>
+  <header class="navbar">
+    <div class="container">
+      <h1 class="logo">Horse Racing</h1>
+      <nav class="nav-links">
+        <a href="#">Home</a>
+        <a href="#">Programs</a>
+        <a href="#">Results</a>
+      </nav>
+      <nav>
+        <BaseButton style="margin-right: 0.5rem">Generate Program</BaseButton>
+        <BaseButton variant="secondary">Start/Pause</BaseButton>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import BaseButton from '../atoms/BaseButton.vue'
+</script>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  background-color: #f9fafb;
+  padding: 0.5rem 1rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #4f46e5;
+}
+
+.nav-links a {
+  margin-right: 1rem;
+  text-decoration: none;
+  color: #374151;
+  font-weight: 500;
+  transition: color 0.2s;
+}
+
+.nav-links a:hover {
+  color: #4f46e5;
+}
+</style>
